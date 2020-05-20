1035fcaab763f32fe39a431f69844ba5b7e5734f
nhudtwalcker <nhudtwalcker@gmail.com>
2020-05-20 10:59:12 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,7 +10,7 @@ Citizen.CreateThread(function()
         local playerPed = GetPlayerPed(-1)
         if IsPedInAnyVehicle(playerPed, false) then
             local playerVeh = GetVehiclePedIsIn(playerPed, false)
-            local vehPlates = "TSTPLATE1"
+            local vehPlates = "TSTPLATE1" --hard coded just to test
             TriggerServerEvent("servertest:sendInfo", vehPlates) --this trigger never happens even though criteria is met
 
         end
